51a9fe3d7bf08da7c6f55d52d89505d7aa41595d
rubennmg <rubenmg2015@gmail.com>
2024-01-20 21:11:06 +0100

    implemented header v1.0

---
+++ notes.md
@@ -0,0 +1,14 @@
+# Main pages
+- Home
+- About
+- Formation
+- Projects
+- Contact
+
+# More
+- 404 not found own page
+- in development
+
+# ToDo
+- Componente para elmento de navbar y sustituirlo por ``<a>``
+- footer
---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			'df-text': '#eeefe9',
+			'background': '#171911',
+			'navbar': '#0f100b',
+			'primary': '#bcbfa8',
+			'secondary': '#424c5a',
+			'accent': '#8a7b9e',
+		},
+	},
+	plugins: [],
+}
