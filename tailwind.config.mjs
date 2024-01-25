bfc2e5312d049d12b7d5bdbaf29b86f20340d68f
rubennmg <rubenmg2015@gmail.com>
2024-01-25 20:45:12 +0100

    about me section + upgrades

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,11 +4,12 @@ export default {
 	theme: {
 		colors: {
 			'df-text': '#eeefe9',
-			'background': '#171911',
+			'background': '#1E1E23',
 			'navbar': '#0f100b',
 			'primary': '#bcbfa8',
 			'secondary': '#424c5a',
 			'accent': '#8a7b9e',
+			'border': '#f1aa4e',
 		},
 	},
 	plugins: [],
